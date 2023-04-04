b65a9395b8a369b714343e78e296f76687e55d6a
Justin Sweeten <jtsweeten@gmail.com>
2023-04-04 15:37:12 -0500

    I DID IT OMG

---
+++ GoalSetter/Models/UserType.cs
@@ -0,0 +1,8 @@
+﻿namespace GoalSetter.Models
+{
+    public class UserType
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ GoalSetter/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,16 @@
+﻿using GoalSetter.Models;
+using System.Collections.Generic;
+
+namespace GoalSetter.Repositories
+{
+    public interface IUserProfileRepository
+    {
+        void Add(UserProfile userProfile);
+        UserProfile GetByFirebaseUserId(string firebaseUserId);
+        List<UserProfile> GetUsers();
+        UserProfile GetById(int id);
+        void Update(UserProfile userProfile);
+        List<UserType> GetUserTypes();
+        void Remove(int id);
+    }
+}
